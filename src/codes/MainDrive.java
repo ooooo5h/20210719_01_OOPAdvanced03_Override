90b60d768cbb2232388a8b7b382d181d6b07d823
전은형 <ooooo5h91@gmail.com>
2021-07-20 21:47:27 +0900

    클래스를 syso로 찍으면, 자동으로 실행되는 toString()

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -4,6 +4,15 @@ public class MainDrive {
 	
 	public static void main(String[] args) {
 		
+//		Dog를 객체화
+		Dog d1 = new Dog();
+		
+		
+		System.out.println(d1);
+//		클래스를 찍었더니 codes.Dog@4361bd48이라고 나왔다
+//		참조형 변수(Dog)를 syso로 출력하면 자동으로 실행되는 toString() 기능!
+//		참조형 변수는 찾아가야할 번지수를 담아두는데, 그 번지수의 주소가 사람이 알아볼 수 있는 문장으로 변환되는 기능!
+		
 		
 	}
 
